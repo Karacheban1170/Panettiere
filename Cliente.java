5db08a5d6ab3f2466015dc67b5305c35c949dbb6
Karacheban1170 <mykyta.karacheban@gmail.com>
2024-10-16 21:35:27 +0200

    34

--- Cliente.java
+++ Cliente.java
@@ -1,22 +1,14 @@
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Cliente implements Runnable {
-    private Panificio panificio;
-    private final int CAPACITA = 5;  // Massimo numero di pani nel panificio
-    private final Queue<String> pani = new LinkedList<>();
-
-    public Cliente(Panificio panificio) {
-        this.panificio = panificio;
-    }
+    private final int CAPACITA = 5;
+    private final LinkedList<String> pani = new LinkedList<>();
 
     @Override
     public void run() {
         try {
             while (true) {
-                // Il cliente acquista pane
                 acquistarePane();
-                // Attende un po' prima di ritornare
                 Thread.sleep(3000);
             }
         } catch (InterruptedException e) {
@@ -24,14 +16,11 @@ public class Cliente implements Runnable {
         }
     }
 
-    // Metodo sincronizzato per acquistare pane
     public synchronized void acquistarePane() throws InterruptedException {
         while (pani.isEmpty()) {
-            // Se non c'è pane, il cliente attende
             System.out.println("Niente pane! Attesa del cliente...");
             wait();
         }
-        // Il cliente prende un pane
         pani.poll();
         System.out.println("Cliente: Pane acquistato!");
         notifyAll();
--- Panettiere.java
+++ Panettiere.java
@@ -1,23 +1,15 @@
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Panettiere implements Runnable {
-    private final Panificio panificio;
-    
-    private final int CAPACITA = 5;  // Massimo numero di pani nel panificio
-    private final Queue<String> pani = new LinkedList<>();
 
-    public Panettiere(Panificio panificio) {
-        this.panificio = panificio;
-    }
+    private final int CAPACITA = 5; 
+    private final LinkedList<String> pani = new LinkedList<>();
 
     @Override
     public void run() {
         try {
             while (true) {
-                // Il panettiere produce pane
                 produrrePane();
-                // Attende un po' prima di produrre altro pane
                 Thread.sleep(2000);
             }
         } catch (InterruptedException e) {
@@ -25,18 +17,14 @@ public class Panettiere implements Runnable {
         }
     }
 
-    // Metodo sincronizzato per produrre pane
     public synchronized void produrrePane() throws InterruptedException {
         while (pani.size() == CAPACITA) {
-            // Se il panificio è pieno, il panettiere attende
             System.out.println("Panificio pieno! Attesa...");
             wait();
         }
-        // Aggiungiamo pane e notifichiamo i clienti
+
         pani.add("Pane");
         System.out.println("Panettiere: Pane prodotto!");
         notifyAll();
     }
 }
-
-
